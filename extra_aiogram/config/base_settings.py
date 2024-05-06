4880bdd45ca6838631b54308574c3e8bdf6d58df
Lev <mls@rio-soft.ru>
2024-05-06 18:47:20 +0500

    Upd logic settings

--- extra_aiogram/config/base_settings.py
+++ extra_aiogram/config/base_settings.py
@@ -1,7 +1,21 @@
 from os import getenv
 
 
+def get_boolean_env_var(env_var_name, default=False):
+    value = getenv(env_var_name, str(default)).strip().lower()
+    return value in ['true', '1', 't', 'y', 'yes']
+
+
+def get_list_env_var(env_var_name, default="", separator=','):
+    value = getenv(env_var_name, default)
+    return [item.strip() for item in value.split(separator) if item.strip()]
+
+
 class BaseSettings:
+
+    def __init__(self):
+        raise NotImplementedError("You must define settings parameters.")
+
     @property
     def DATABASE_URL(self):
         raise NotImplementedError("You must provide a DATABASE_URL.")
@@ -12,8 +26,10 @@ class BaseSettings:
 
 
 class DefaultSettings(BaseSettings):
-    CUSTOM_BASE_MODULE = getenv('CUSTOM_BASE_MODULE')
-    DEBUG = getenv('DEBUG', False)
+    def __init__(self):
+        self.CUSTOM_BASE_MODULE = getenv('CUSTOM_BASE_MODULE')
+        self.DEBUG = get_boolean_env_var('DEBUG')
+        self.DEFAULT_MODELS = get_list_env_var('DEFAULT_MODELS', 'Chat, User, Message, GroupInfo, Group')
 
     @property
     def DATABASE_URL(self):
