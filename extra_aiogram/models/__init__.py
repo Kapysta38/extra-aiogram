21f84254e24ffe60817b4853416794d25a6c6d7a
Lev <mls@rio-soft.ru>
2024-05-06 19:08:21 +0500

    Added comments

--- extra_aiogram/models/__init__.py
+++ extra_aiogram/models/__init__.py
@@ -12,7 +12,6 @@ from .messages import Message
 
 metadata = MetaData()
 
-# Создание движка SQLAlchemy, подключенного к базе данных
 engine = create_engine(settings.DATABASE_URL)
 Session = sessionmaker(bind=engine)
 
@@ -21,28 +20,43 @@ model_registry = []
 
 # функция для регистрации моделей
 def register_model(model_class):
+    """
+    Функция для регистрации моделей
+    :param model_class:
+    :return:
+    """
     if model_class not in model_registry:
         model_registry.append(model_class)
         model_class.__table__.tometadata(metadata)
 
 
+# Регистрируем стандартные модели
 for class_name in settings.DEFAULT_MODELS:
     settings_module = importlib.import_module('extra_aiogram.models')
     model = getattr(settings_module, class_name)
     register_model(model)
 
 
-# Функция для создания всех таблиц
 def create_tables():
+    """
+    Создает таблицы, соответствующие зарегистрированным моделям.
+    """
     metadata.create_all(engine)
 
 
-# Функция для удаления всех таблиц (используйте с осторожностью!)
 def drop_tables():
+    """
+    Удаляет все таблицы в базе данных.
+    """
     metadata.drop_all(engine)
 
 
 def initialize_default_groups():
+    """
+    Инициализирует стандартные группы.
+
+    Создает записи о стандартных группах в базе данных.
+    """
     session = Session()
     session.add_all([
         GroupInfo(name='admin'),
